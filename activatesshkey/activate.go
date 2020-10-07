c82402d19acf7d35f4c601e8d9c265a7d6a42616
Laszlo Pusok <laszlo.pusok@bitrise.io>
2020-10-07 13:00:46 +0200

    Using NewStepError to constuct step errors, this allows including step ID

--- activatesshkey/activate.go
+++ activatesshkey/activate.go
@@ -21,51 +21,51 @@ type Config struct {
 func Execute(cfg Config) error {
 	// Remove SSHRsaPrivateKey from envs
 	if err := unsetEnvsBy(string(cfg.SSHRsaPrivateKey)); err != nil {
-		return &StepError{
-			Tag:      "remove-private-key-data",
-			ShortMsg: "Failed to remove private key data from envs",
-			Err:      err,
-		}
+		return NewStepError(
+			"remove-private-key-data",
+			err,
+			"Failed to remove private key data from envs",
+		)
 	}
 
 	if err := ensureSavePath(cfg.SSHKeySavePath); err != nil {
-		return &StepError{
-			Tag:      "create-ssh-save-path",
-			ShortMsg: "Failed to create the provided path",
-			Err:      err,
-		}
+		return NewStepError(
+			"create-ssh-save-path",
+			err,
+			"Failed to create the provided path",
+		)
 	}
 
 	if err := fileutil.WriteStringToFile(cfg.SSHKeySavePath, string(cfg.SSHRsaPrivateKey)); err != nil {
-		return &StepError{
-			Tag:      "write-ssh-key",
-			ShortMsg: "Failed to write the SSH key to the provided path",
-			Err:      err,
-		}
+		return NewStepError(
+			"write-ssh-key",
+			err,
+			"Failed to write the SSH key to the provided path",
+		)
 	}
 
 	if err := os.Chmod(cfg.SSHKeySavePath, 0600); err != nil {
-		return &StepError{
-			Tag:      "change-ssh-key-permission",
-			ShortMsg: "Failed to change file's access permission",
-			Err:      err,
-		}
+		return NewStepError(
+			"change-ssh-key-permission",
+			err,
+			"Failed to change file's access permission",
+		)
 	}
 
 	if err := restartAgent(cfg.IsRemoveOtherIdentities); err != nil {
-		return &StepError{
-			Tag:      "restart-ssh-agent",
-			ShortMsg: "Failed to restart SSH Agent",
-			Err:      err,
-		}
+		return NewStepError(
+			"restart-ssh-agent",
+			err,
+			"Failed to restart SSH Agent",
+		)
 	}
 
 	if err := checkPassphrase(cfg.SSHKeySavePath); err != nil {
-		return &StepError{
-			Tag:      "check-passphrase",
-			ShortMsg: "SSH key requires passphrase",
-			Err:      err,
-		}
+		return NewStepError(
+			"check-passphrase",
+			err,
+			"SSH key requires passphrase",
+		)
 	}
 
 	fmt.Println()
